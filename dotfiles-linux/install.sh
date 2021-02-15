19d17cb1575934cf95d337c6452b698f064a60de
AdvaitDhingra <62119114+AdvaitDhingra@users.noreply.github.com>
2021-02-15 12:05:07 +0100

    Fixed some install errors

--- dotfiles-linux/install.sh
+++ dotfiles-linux/install.sh
@@ -12,7 +12,13 @@ Welcome
 
 echo "Installing all of your programs..."
 
-sudo apt install vscode vlc firefox discord 
+wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+
+sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+
+
+sudo apt install code vlc firefox 
+sudo snap install discord 
 
 echo "Done installing. Enjoy :D"
 
