594eef9032944f6101227455020606ecdb3eba9a
AdvaitDhingra <62119114+AdvaitDhingra@users.noreply.github.com>
2021-03-13 19:42:13 +0100

    Added ROOT

--- install.sh
+++ install.sh
@@ -11,17 +11,22 @@ ln -s ~/.dotfiles/.vscode ~/.vscode
 ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
 
 echo "Installing Brew..."
-sudo apt-get install build-essential curl file git
+
+sudo apt-get install build-essential curl file git python python3
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
-echo "Installing brew cask..."
+git clone https://github.com/root-project/root.git
+mkdir rootbuild rootinstall
+cd rootbuild
+sudo apt-get install dpkg-dev cmake g++ gcc binutils libx11-dev libxpm-dev libxft-dev libxext-dev python libssl-dev
+cmake -DCMAKE_INSTALL_PREFIX=../rootinstall ../root
+cmake --build . -- -j4
+cd bin
+bash thisroot.sh
 
-brew tap homebrew/cask
+echo "ROOT has been installed"
 
-echo "Installing all of your programs..."
-
-brew bundle --file ~/.dotfiles/Brewfile
 
 echo "Done installing. Enjoy :D"
 
