1b1a531badba8b29986d6557c3e44a378aa14544
AdvaitDhingra <62119114+AdvaitDhingra@users.noreply.github.com>
2021-04-22 21:01:16 +0200

    undid screwup

--- setup.sh
+++ setup.sh
@@ -1,4 +1,22 @@
-ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
-ln -a ~/.dotfiles/.bashrc ~/.bashrc
+ln -s ~/.dotfiles/config-files/.gitconfig ~/.gitconfig
 
 echo "Done linking"
+
+echo "Installing ROOT"
+
+sudo apt install git cmake
+
+cd ..
+
+git clone https://github.com/root-project/root
+
+mkdir rootbuild rootinstall
+
+cd rootbuild
+
+cmake -DCMAKE_INSTALL_PREFIX=../rootinstall ../root
+cmake --build . -- -j4
+cd bin
+source thisroot.sh
+
+echo "Done installing root"
